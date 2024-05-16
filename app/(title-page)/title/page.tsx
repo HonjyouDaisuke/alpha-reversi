af2e9a1e0b81b1e2f09c00bda462a3ab7daaca21
HonjyouDaisuke <78740184+HonjyouDaisuke@users.noreply.github.com>
2024-05-16 17:55:34 +0900

    TitleBoardリファクタリング

--- app/(title-page)/title/page.tsx
+++ app/(title-page)/title/page.tsx
@@ -1,52 +1,29 @@
 "use client";
 import Button from "@/components/button";
-import { Player } from "@/entity/player/player-type";
-import titleImage from "@/img/osero-illust17.png";
-import titleStrImage from "@/img/title.png";
-//import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import CheckBox from "@/components/checkbox";
 import SelectCharactor, { FirstSecondHand } from "@/components/select-charactor";
 import TitleImage from "@/components/title-image";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export default function TitleBoard() {
 	const [playerA, setPlayerA] = useState(0);
 	const [playerB, setPlayerB] = useState(0);
 	const [useHint, setUseHint] = useState(false);
-
 	const router = useRouter();
-	function handleChangePlayerA(event: React.ChangeEvent<HTMLSelectElement>) {
-		setPlayerA(Number(event.target.value));
-	}
 
-	function handleChangePlayerB(event: React.ChangeEvent<HTMLSelectElement>) {
-		setPlayerB(Number(event.target.value));
-	}
-
-	function handleButton() {
-		const params = new URLSearchParams();
-		params.append("playerA", String(playerA));
-		params.append("playerB", String(playerB));
-		params.append("useHint", String(useHint));
-
-		const href = `/reversi?${params}`;
-		router.push(href);
-	}
-
+	const handleButton = () => navigateToReversi(router, playerA, playerB, useHint);
 	function handleHint() {
-		console.log(`useHint = ${useHint}`);
 		setUseHint(!useHint);
 	}
-
 	return (
 		<div className="h-screen w-screen flex bg-gray-600 items-center justify-center">
 			<div className="bg-white rounded p-4 text-black shadow">
 				<TitleImage />
 				<div className="grid grid-cols-2">
-					<SelectCharactor firstSecond={FirstSecondHand.First} onChange={handleChangePlayerA} />
-					<SelectCharactor firstSecond={FirstSecondHand.Second} onChange={handleChangePlayerB} />
+					<SelectCharactor firstSecond={FirstSecondHand.First} onChange={(e) => setPlayerA(Number(e.target.value))} />
+					<SelectCharactor firstSecond={FirstSecondHand.Second} onChange={(e) => setPlayerB(Number(e.target.value))} />
 				</div>
 				<div className="flex w-full justify-center items-center gap-4 p-3">
 					<CheckBox onChange={handleHint} value={useHint}>
@@ -60,3 +37,13 @@ export default function TitleBoard() {
 		</div>
 	);
 }
+
+const navigateToReversi = (router: AppRouterInstance, playerA: number, playerB: number, useHint: boolean) => {
+	const params = new URLSearchParams();
+	params.append("playerA", String(playerA));
+	params.append("playerB", String(playerB));
+	params.append("useHint", String(useHint));
+
+	const href = `/reversi?${params}`;
+	router.push(href);
+};
