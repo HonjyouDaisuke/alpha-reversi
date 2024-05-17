21df02af44f22cbb18217e83ba3cdb6dae6b145b
HonjyouDaisuke <78740184+HonjyouDaisuke@users.noreply.github.com>
2024-05-17 13:23:32 +0900

    console.logを削除しました。

--- app/usecase/turn-controller.ts
+++ app/usecase/turn-controller.ts
@@ -4,46 +4,41 @@ import { Turn } from "../entity/turn/turn";
 import { TurnType } from "../entity/turn/turn-type";
 
 export class TurnController {
-  turn = new Turn();
-  playerId: number[] = [];
-  piece: CellType[] = [];
+	turn = new Turn();
+	playerId: number[] = [];
+	piece: CellType[] = [];
 
-  constructor() {}
+	constructor() { }
 
-  setPlayer(playerA: number, playerB: number) {
-    this.playerId[0] = playerA;
-    this.playerId[1] = playerB;
-    this.piece[0] = CellType.Black;
-    this.piece[1] = CellType.White;
-  }
+	setPlayer(playerA: number, playerB: number) {
+		this.playerId[0] = playerA;
+		this.playerId[1] = playerB;
+		this.piece[0] = CellType.Black;
+		this.piece[1] = CellType.White;
+	}
 
-  changeTurn() {
-    this.turn.printCurrentTurn();
-    this.turn = this.turn.changeTurn();
-    this.turn.printCurrentTurn();
-  }
+	changeTurn() {
+		this.turn = this.turn.changeTurn();
+	}
 
-  getCurrentTurn(): TurnType {
-    return this.turn.getCurrentTurn();
-  }
+	getCurrentTurn(): TurnType {
+		return this.turn.getCurrentTurn();
+	}
 
-  getCurrentTurnCell(): CellType {
-    if (this.turn.getCurrentTurn() === TurnType.TurnA) return CellType.Black;
-    else return CellType.White;
-  }
+	getCurrentTurnCell(): CellType {
+		if (this.turn.getCurrentTurn() === TurnType.TurnA) return CellType.Black;
+		else return CellType.White;
+	}
 
-  getCurrentPlayerId(): number {
-    return this.playerId[this.turn.getCurrentTurn()];
-  }
+	getCurrentPlayerId(): number {
+		return this.playerId[this.turn.getCurrentTurn()];
+	}
 
-  getPlayerAId(): number {
-    return this.playerId[0];
-  }
+	getPlayerAId(): number {
+		return this.playerId[0];
+	}
 
-  getPlayerBId(): number {
-    return this.playerId[1];
-  }
-  printCurrentTurn() {
-    this.turn.printCurrentTurn();
-  }
+	getPlayerBId(): number {
+		return this.playerId[1];
+	}
 }
