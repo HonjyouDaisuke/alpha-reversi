9e2bbfc44561a05a89efe10b5486d5a8bb98e60a
HonjyouDaisuke <78740184+HonjyouDaisuke@users.noreply.github.com>
2024-06-10 10:48:45 +0900

    履歴表示を逆順にしました。

--- app/components/right-side-bar.tsx
+++ app/components/right-side-bar.tsx
@@ -5,13 +5,15 @@ interface Props {
 	logData: LogType[];
 }
 export function RightSideBar({ logData }: Props) {
+	const reversedLogData = logData.slice().reverse();
+
 	return (
 		<div className="h-full p-3">
 			<div className="flex flex-col w-full h-full bg-gray-300 rounded shadow">
 				<div className="text-lg font-bold p-3 text-black">★対戦履歴★</div>
 				<div className="flex flex-col gap-2 w-full h-full px-3 overflow-auto">
 
-					{logData.map((item, index) => (
+					{reversedLogData.map((item, index) => (
 						<LogCard key={index} logData={item} />
 					))}
 
